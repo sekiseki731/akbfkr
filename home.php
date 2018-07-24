4de142de45a1a4329d463d85ad942368940eda2e
sekiseki <sekiseki@local>
2018-07-24 14:18:59 +0530

    calender has done

--- home.php
+++ home.php
@@ -12,7 +12,209 @@
 
 <?php get_header(); ?>
 
-	<div id="main" class="main-content-area">
+    <div id="main" class="main-content-area">
+
+        <!-- from here you can write main content-->
+            <p class="reservdBtn"><a href="#">RESERVATION</a></p>
+    
+                        
+                        
+                        
+            <div class="reserveAreaWrap">
+                <div class="calenderPager cf">
+                    <ul>
+			
+						<li class="pagerNext"><a href="http://akiba2960.com/reserved/?date=2018-08-07">NEXT<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div> <!-- calenderPager -->
+        
+                <div class="scroll">
+                <table class="reserveFormTable" border="1">
+                    <tbody>
+                        <tr class="yymm">
+                            <td colspan="9">2018/07</td>
+                            <td colspan="6">2018/08</td>
+  		  		        </tr>
+  		                <tr>
+                            <td></td>
+                            <td class="day">24<span>(tue)</span></td>
+                            <td class="day">25<span>(wed)</span></td>
+                            <td class="day">26<span>(thu)</span></td>
+                            <td class="day">27<span>(fri)</span></td>
+                            <td class="day">28<span>(sat)</span></td>
+                            <td class="day">29<span>(sun)</span></td>
+                            <td class="day">30<span>(mon)</span></td>
+                            <td class="day">31<span>(tue)</span></td>
+                            <td class="day">1<span>(wed)</span></td>
+                            <td class="day">2<span>(thu)</span></td>
+                            <td class="day">3<span>(fri)</span></td>
+                            <td class="day">4<span>(sat)</span></td>
+                            <td class="day">5<span>(sun)</span></td>
+                            <td class="day">6<span>(mon)</span></td>
+                        </tr>
+
+                        <tr class="statusArea">
+  		  		  			<td class="time">12:00〜13:00</td>
+  		
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		      <tr class="statusArea">
+  		  		  			<td class="time">13:00〜14:00</td>
+  		
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		      <tr class="statusArea">
+  		  		  			<td class="time">14:00〜15:00</td>
+
+                                    <td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		      <tr class="statusArea">
+  		  		  			<td class="time">15:00〜16:00</td>
+  		
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		      <tr class="statusArea">
+  		  		  			<td class="time">16:00〜17:00</td>
+  		
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		      <tr class="statusArea">
+  		  		  			<td class="time">17:00〜18:00</td>
+  		
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		      <tr class="statusArea">
+  		  		  			<td class="time">18:00〜19:00</td>
+  		
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status reserveOnTd"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>
+  			  		  				<td class="status"></td>  			
+  			  		  				<td class="status"></td>
+                                    <td class="status"></td> 
+  			  		</tr>
+  		
+			<!--//ここにコメントデータ-->
+
+			<!--
+			<tr>
+
+				<td>キャンペーン情報</td>
+									<td class="campaign">
+						<span class="c_text-" style="display:none;">
+														</span>
+					</td>
+							</tr>
+-->
+						<tr style="background: #fff;">
+			<td colspan="15" style="color: ##959595;font-weight: 600;">
+					Available：<span style="color: #f2395b;padding-right: 15px;">◯</span>
+			Little remainder：<span style="color: #f2395b;padding-right: 15px;">△</span>
+			No vacant tables：<span style="color: #000;">×</span>
+		
+				</td>
+			</tr>
+  	</tbody></table>
+		<div id="c_ap">
+			<!--  キャンペーンテキスト出力  -->
+		</div>
+	</div>
+</div>
+        
+    <div class="topText">
+        <p>AkibaFukurou is an Owlcafe located in Akihabara, Tokyo：the number of guests will be【limited to small groups】also our room is provided【by reservation only】</p>
+        <p>People who are busy or from overseas can enter the room smoothly without standing in a big queue.</p>
+        <p>Please pick the date/time on the calendar you wish to make your reservation by clicking a circle or a triangle mark below,and proceed to the reservation form.</p>
+    </div>
+
+                        
+<!-- main content おわり-->                        
 
 		<div class="component-inner">
 
@@ -50,8 +252,6 @@
 
 						<?php endif; // is_active_sidebar( 'sidebar-6' ) ?>
                         
-<!-- from here you can write main content-->
-  <p class="reservdBtn"><a href="http://akiba2960.com/reserved/">RESERVATION</a></p> 
                     
                     </div><!-- #topmain-widget-area -->
 
