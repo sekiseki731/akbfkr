8a3dc2f1ffa98b2bb9bcd862f42eddb5ede31902
sekiseki <sekiseki@local>
2018-07-26 15:59:43 +0530

    content has done!

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
 <meta name="viewport" content="width=device-width" />
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
 <?php wp_head(); ?>
+</head>
 <body <?php body_class(); ?>>
 
 
--- home.php
+++ home.php
@@ -215,63 +215,311 @@ s
         <h2 class="primTitle">FAMILY</h2>
 
         
-        <div class="slick-track" style="opacity: 1; width: 7480px; transform: translate3d(-2380px, 0px, 0px);"><div class="slick-slide" style="width: 150px;">
-            <a href="#">
-              <div class="imgBlock">1</div>
-                <div class="textArea">
-                    <h3>鳥の種類<span>名前</span></h3>
-                    <p></p>
-                </div>
-            </a>
-          </div>
-        <div class="slick-slide" style="width: 150px;">
-            <a href="#">
-              <div class="imgBlock">2</div>
-                <div class="textArea">
-                    <h3>鳥の種類<span>名前</span></h3>
-                    <p></p>
-                </div>
-            </a>
-          </div>
-        <div class="slick-slide" style="width: 150px;">
-            <a href="#">
-              <div class="imgBlock">3</div>
-                <div class="textArea">
-                    <h3>鳥の種類<span>名前</span></h3>
-                    <p></p>
-                </div>
-            </a>
-          </div>
-        <div class="slick-slide" style="width: 150px;">
-            <a href="#">
-              <div class="imgBlock">4</div>
-                <div class="textArea">
-                    <h3>鳥の種類<span>名前</span></h3>
-                    <p></p>
-                </div>
-            </a>
-          </div>
-        <div class="slick-slide" style="width: 150px;">
-            <a href="#">
-              <div class="imgBlock">5</div>
-                <div class="textArea">
-                    <h3>鳥の種類<span>名前</span></h3>
-                    <p></p>
-                </div>
-            </a>
-          </div>
-        </div>
-
+        <?php echo do_shortcode('[smartslider3 slider=3]');?>
         
-        <div class="goods_container">
+        <div class="article_container">
         <h2 class="primTitle">Owlcafe "Akiba Fukurou" opens from Aug.8th,2014</h2>
         <div class="topText">
             <p>Now we have 36 owls due to so many guests, mainly foreigners of all over the world.</p>
             <p>We have created this unique space so that owls and people can meet each other.</p>
         </div>
             
-       
-<!-- main content おわり-->
+        <div class="img_family">
+            <img class="wide_img" src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/AkibaFukurou_family-1.jpg"  alt="family">
+        </div>
+            
+        <div class="goods_container">
+        <h2 class="primTitle">ORIGINAL GOODS</h2>
+            <?php 
+echo do_shortcode('[smartslider3 slider=4]');
+?>
+            </div>
+            
+        <div class="gotoBtn">
+            <a href="#">MORE GOODS</a>
+        </div>
+            
+            
+        <div class="article_container topText">
+            <h2 class="primTitle">ROOMS</h2>         <p>AkibaFukurou is a wonder room which many owls are in,</p>
+            <p>surrounded by lots of chandeliers with relaxing Mozart music. </p>
+            <p>We hope all of you will experience the graceful, special moments.</p>
+            <img class="wide_img" src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A1291-2.jpg">
+        </div>
+            
+        <div class="article_container">
+            <h2 class="primTitle">OUR PHILOSOPHY</h2>
+            <h3>[To live more fulfilling life through interaction with owls] It's our philosophy.</h3>
+            <div class="topText">
+                <p>Interacting with relaxed-adorable owls make many people smile, and we think owls can heal our tired hearts like a therapy.</p>
+                <p>We prioritize to keep our owls in good health ,and then, we apply the healing effects of 《Classical Music,Sconce Lighting,and White Color》 to our room so that our guests can experience "extraordinary healing".</p>
+                <p>As a part of our perfomance, every flow of background music has its own theme, we go along with the story for 1 hour.</p>
+                <p>The number of guests will be【limited to small gruops,and our room is provided by reservation only】so that the guests can spend some cozy time.</p>
+                <p>We hope you will enjoy the “healing entertament” which is the only one bizarre-space in the world ♪</p>
+            </div>
+            <img class="wide_img" src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/MG_6319-2.jpg">
+            </div>
+            
+            
+            <div class="memories_container">
+                <h2 class="primTitle">MAKE PRECIOUS MEMORIES<br>
+                WITH YOUR LOVED ONE</h2>
+                
+                <ul class="memories_list">
+                    <li>
+                        <a href="#">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1-2.jpg">
+                            <p>POPULAR TOURIST ATTRACTION<br>
+                            TOP1★IN TOKYO</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/2-2.jpg">
+                            <p>CLEAN PLACE<br>LIMITTED TO SMALL GROUPS<br>35OWLS ★ 33SPECIES<br>Keeping the owls' living environment cozy is our top priorty.</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/3.jpg">
+                            <p>PLEASE ENJOY<br>''THE ULTIMATE OWL EXPERIENCE''<br>Background Music flow has its own story.</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="article_container">
+                <img class="wide_img" src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A7997-1.jpg">
+                <div class="centerText">
+                    <h2>WELCOME TO OUR FAMILY♥</h2>
+                    <p>We hope you will enjoy your time with our cute owls and you will feel like becoming a member of Akiba Fukurou.</p>
+                </div>
+            </div>
+            
+            <div class="article_container">
+                <h2 class="primTitle">A MAGIC ENCOUNTER<br>
+〜AKIBA FUKUROU TALE〜</h2>
+                <p></p>
+            </div>
+            <div class="content_wrapper">
+            <ul>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A7971.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[1] 〜PROLOGUE〜<br>
+                            Please read carefully the Q&A before get in.</h3>
+                        <p>It contains all owls'profiles and a lot of informations about them. The booklet weighs 2 pounds and it takes around 20 minutes to read it.</p>
+                    </div>
+                </li>  
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A5573.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[2] 〜Opening act〜</h3>
+                        <p>An owl specialist will have a little lecture before you meet your new friends.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A1970.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[3] 〜The Performance〜</h3>
+                        <p>Enjoy the atmosphere with owls and candelabra, with the sound of Mozart's music it's like a movie.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A2146.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[4] Take pictures of owls!</h3>
+                        <p>as much as you want.♪</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A4291.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[5] Check your favorite owls' names.</h3>
+                        <p>Some names are original. You can also find their birthday and personality description.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A1990-2.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[6] Touch owls gently.</h3>
+                        <p>You can try out their softness.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A2078.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[7] Hold an owl on your arm.</h3>
+                        <p>and enjoy this unique moment.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/IMG_8378.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[8] You will have your picture taken by our photographer for a memorable souvenir!</h3>
+                        <p>Camera body：CANON EOS 5D Mark IV</p>
+                        <p>Lens：CANON EF35mm F1.4L II USM</p>
+                        <p>　　：CANON EF16-35mm F2.8L II USM</p>
+                        <p>　　：CANON EF50mm F1.2L USM</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A5428.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[9] As a souvenir we will give you a beautiful picture♥</h3>
+                        <p>※The frame design changes everyday.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A4382.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[10] Goods and souvenirs.</h3>
+                        <p>You will find a lot of original Fukuro Cafe products: feathers, postcards, calendars, masking tapes...You can only find them in Akiba Fukurou.</p>
+                    </div>
+                </li>
+                <li class="content_bottom_border">
+                    <div class="content_img">
+                        <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A4979.jpg">
+                    </div>
+                    <div class="content_text">
+                        <h3>[11] Boost your energy with owls♥</h3>
+                        <p>Owls will help you to keep your smile!</p>
+                    </div>
+                </li>
+                </ul>
+            </div>
+            
+            <div class="article_container">
+                <h2 class="primTitle">CUSTOMER’S VOICE</h2>
+                <div class="content_wrapper">
+                <ul>
+                    <li>
+                        <div class="content_img">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A7971.jpg">
+                        </div>
+                        <div class="content_text topReview">
+                            <a href="#">
+                            <h2>
+                              <span>◉◉◉◉◉</span>Perfect for animal lovers!
+                            </h2>
+                            <p>This was my favorite part of Tokyo. What separates this from the other animal cafes is the owl’s welfare comes first. The owners take great pride and care of the owls making their environment relaxing and stress free. Reservations are a must and can be done easily online. The atmosphere is quiet, comfortable with only a limited amount of people per hour allowed in the cafe. There are plenty of owls ranging in sizes and breeds and all of them seemed relaxed and stress free. Seeing them taken care of and respected by the owners made me feel good about being there. The owners are very kind and patient and it’s clear their number one concern is the safety of the animals. I’m so glad we chose this place. Other animal cafes in Tokyo have bad ratings based on the treatment of the animals by the owners (shockingly) and being over crowded with people. This is not the case at Akiba Fukurou. We walked out so excited we had the chance to experience this. Remember to make reservations a few days in advance. It’s an experience you’ll treasure for a lifetime.</p>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="content_img">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A7971.jpg">
+                        </div>
+                        <div class="content_text topReview">
+                            <a href="#">
+                            <h2>
+                              <span>◉◉◉◉◉</span>
+                                A Heartwarming Experience! Went to multiple owl cafes and this was by far the best.
+                            </h2>
+                            <p>I don’t know if I can fully express how wonderful my experience here was. The owls are obviously well cared for, the atmosphere is very calm, and the hosts are extremely gracious and make sure you fully understand how to interact with the owls in the way that will make both the person and bird most comfortable.The owls are well cared for, happy and relaxed. They do not "overwork" or stress their owls.The staff is very attentive and it is evident that they care about the animals well being first and foremost.Overall the experience was amazing and I felt comfortable with the experience and the treatment of the owls. A definite must-do while in Japan!</p>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="content_img">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A7971.jpg">
+                        </div>
+                        <div class="content_text topReview">
+                            <a href="#">
+                            <h2>
+                              <span>◉◉◉◉◉</span>最高の楽園でした！
+                            </h2>
+                            <p>フクロウカフェが大好きで今までたくさんのお店に行きましたが、断トツで一番素晴らしかったです。こちらのお店では、顧客に最大限の癒しを与えようという・楽しませようという配慮はもちろん、フクロウさんに対しての配慮がどのお店よりも行き届いていました。フクロウカフェは本来こうあるあるべきだ、と感動しました。いのちを扱う、ということを重く受け止め、ただのキャストとしてでなく、同じ生命として尊重していることが至る所から感じられました。寄り添って身を委ねてくれたり、腕に乗せてすぐさまうとうとと目を閉じてくれたり、羽繕いをしてくれたり、繊細なフクロウさんがこんなにもリラックスした姿を見せてくれるのは、ひとえにスタッフさんの努力と顧客のマナーあってのものだと思います。そんな環境で過ごしているフクロウさんたちは、どの子もとても可愛く、美しく、愛らしく、個性的な魅力に溢れていました。本当に感動しました。スタッフさんのホスピタリティがとにかく素晴らしく、些細な言動からフクロウさんへの愛情が溢れ出ていて、そんな空間で過ごさせていただけてすごく幸せでした。最高の癒しをありがとうございました。</p>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="content_img">
+                            <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/1W7A7971.jpg">
+                        </div>
+                        <div class="content_text topReview">
+                            <a href="#">
+                            <h2>
+                              <span>◉◉◉◉◉</span>很棒的貓頭鷹體驗
+                            </h2>
+                            <p>白色的裝潢、擺飾和店員親切的服務態度，讓人感到舒適溫馨。一進到店裡，左一看、又一看、上看、下看，到處都是貓頭鷹，處處是驚喜！原以為貓頭鷹都長得差不多，直到這次透過近距離的接觸，才發現原來牠們都很有自己的特色喔。最貼心的是，店家會主動幫每位來訪的客人拍下與貓頭鷹的合照，並在離開前贈送給客人，為這趟旅程留下紀念，真的很棒！非常推薦給喜歡貓頭鷹的人喔~</p>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            
+            <div class="blog_container">
+                <h2 class="primTitle">BLOG</h2>
+                <div class="blog_content">
+                <li>
+                    <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/20180606-6D0A9881-340x230.jpg">
+                    <div class="blogText">
+                        <p class="day">2018.07.25</p>
+                        <h3 class="articleTitle">KuppiHoney bee</h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/20180606-6D0A0067-340x230.jpg">
+                    <div class="blogText">
+                        <p class="day">2018.07.24</p>
+                        <h3 class="articleTitle">KUPPI♥︎クッピー</h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/AkibitFukurou-340x230.jpg">
+                    <div class="blogText">
+                        <p class="day">2018.07.23</p>
+                        <h3 class="articleTitle">The 4th anniversary [Aki…</h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="http://localhost:8888/wp-2/wp-content/uploads/2018/07/20180305-1W7A4924-2-340x230.jpg">
+                    <div class="blogText">
+                        <p class="day">2018.07.22</p>
+                        <h3 class="articleTitle">Happy 2nd Birthday♥POT ST…</h3>
+                    </div>
+                </li>
+                </div>
+                
+                <div class="gotoBtn">
+                    <a href="#">MORE BLOG</a>
+                </div>
+            </div>
+            
+            <div class="article_container">
+                <h2 class="primTitle">ACCESS</h2>
+                <div class="access_container">
+                    <p>【By Reservation only】</p>
+                    <p>〒101-0022 67, Neribeicho, Kanda, Chiyodaku, Tokyo</p>
+                    <p>Akiba Fukurou is a world with no phones. Please take some time to contact us through the website.</p>
+                    <p class="access_map">Google map Here!
+                    </p>
+                </div>
+                
+            </div>
+                <!-- main content おわり-->
             
 
 		<div class="component-inner">
